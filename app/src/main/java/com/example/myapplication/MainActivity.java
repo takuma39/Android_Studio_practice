d31f521a5bbd06b496657e28a8f203359a21439f
takuma39 <takuma.sato.3939@gmail.com>
2020-12-30 01:28:27 +0900

    section7_79

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickButton(android.view.View view) {
+        TextView bmiScore =(TextView)this.findViewById(R.id.bmiScore);
+        EditText heightForm = (EditText)this.findViewById(R.id.heightForm);
+        EditText weightForm = (EditText)this.findViewById(R.id.weightForm);
+
+        String strHeight, strWeight;
+        strHeight = heightForm.getText().toString();
+        strWeight = weightForm.getText().toString();
+
+        double bmi, height, weight;
+        height = Double.parseDouble(strHeight);
+        weight = Double.parseDouble(strWeight);
+
+        height /= 100;
+        bmi = weight / (height * height);
+        String msg = "BMIスコア：" + bmi;
+
+        bmiScore.setText(msg);
     }
 }
